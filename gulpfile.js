2eba3ab38d8265a104ac931955700f0d165ad7e2
Aleš Mráz <alik810@seznam.cz>
2016-08-24 16:41:52 +0200

    inicialize project

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+gulp.task('autoprefixer', function() {
+  gulp.src('app/css/styles.css')
+   .pipe(autoprefixer({
+   browsers: ['last 5 versions'],
+   cascade: false
+  }))
+  .pipe(gulp.dest('dist/css'))
+});
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+   .pipe(sass())
+   .pipe(gulp.dest('app/css'))
+   .pipe(browserSync.reload({
+    stream: true
+  }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+   server: {
+   baseDir: 'app'
+  },
+  })
+})
+
+
+
+gulp.task('watch', ['browserSync', 'sass', 'autoprefixer'], function (){
+ gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/css/*.css', ['autoprefixer']);
+ // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+}); 
